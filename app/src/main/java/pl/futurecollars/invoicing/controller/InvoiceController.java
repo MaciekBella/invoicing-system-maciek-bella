33a611e41b43ea680e89e619ffa2b0c513cfe8b7
Michal Caber <javacaber@gmail.com>
2022-12-30 18:32:59 +0100

    Lesson 8 task 1-5

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,20 +1,19 @@
 package pl.futurecollars.invoicing.controller;
 
+import io.swagger.annotations.Api;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.futurecollars.invoicing.api.InvoiceApi;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
-public class InvoiceController {
+@Api(tags = "invoice-controller")
+public class InvoiceController implements InvoiceApi {
 
   private final InvoiceService invoiceService;
 
@@ -23,27 +22,27 @@ public class InvoiceController {
     this.invoiceService = invoiceService;
   }
 
-  @PostMapping("/invoice")
+  @Override
   public long add(@RequestBody Invoice invoice) {
     return invoiceService.save(invoice);
   }
 
-  @GetMapping("/invoices")
+  @Override
   public List<Invoice> getAllInvoices() {
     return invoiceService.getAll();
   }
 
-  @GetMapping("/invoice/{id}")
+ @Override
   public Optional<Invoice> getById(@PathVariable long id) {
     return invoiceService.getById(id);
   }
 
-  @PutMapping("/invoice/{id}")
+  @Override
   public void update(@PathVariable long id, @RequestBody Invoice invoice) {
     invoiceService.update(id, invoice);
   }
 
-  @DeleteMapping("/invoice/{id}")
+ @Override
   public void delete(@PathVariable long id) {
     invoiceService.delete(id);
   }
