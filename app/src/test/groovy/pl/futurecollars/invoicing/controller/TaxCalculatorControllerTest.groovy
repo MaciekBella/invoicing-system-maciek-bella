4af38380e9e9d9311affbfc43ef9679b6a76f5f5
Maciek <maciejbella@op.pl>
2023-01-20 18:38:00 +0100

    lesson 9 - Test Tax Calculator Controller

--- app/src/test/groovy/pl/futurecollars/invoicing/controller/TaxCalculatorControllerTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/controller/TaxCalculatorControllerTest.groovy
@@ -3,73 +3,65 @@ package pl.futurecollars.invoicing.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import pl.futurecollars.invoicing.model.Invoice
-import pl.futurecollars.invoicing.service.TaxCalculatorService
+import pl.futurecollars.invoicing.TestHelpers
+import pl.futurecollars.invoicing.db.DataBase
+import pl.futurecollars.invoicing.service.TaxCalculatorResult
 import pl.futurecollars.invoicing.utils.JsonService
 import spock.lang.Specification
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import static pl.futurecollars.invoicing.TestHelpers.invoice
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class TaxCalculatorControllerTest extends Specification {
 
     @Autowired
-    private TaxCalculatorService taxCalculatorService;
-    @Autowired
     private JsonService jsonService
+    @Autowired
+    private DataBase dataBase
     @Autowired
     private MockMvc mockMvc
 
-    def addInvoiceAndReturnId(String invoiceAsJson) {
-        mockMvc.perform(
-                post("/invoices")
-                        .content(invoiceAsJson)
-                        .contentType(MediaType.APPLICATION_JSON)
-        )
+
+    def "should return sum of products if NIP matches"() {
+        given:
+        def invoice = TestHelpers.invoiceWithNIP("5")
+        dataBase.save(invoice)
+        def url = "/tax/5"
+        when:
+        def result = mockMvc.perform(get(url))
                 .andExpect(status().isOk())
                 .andReturn()
                 .response
                 .contentAsString
-
-    }
-            private List<Invoice> addInvoices(int count) {
-                (1..count).collect { id ->
-                    def invoice = invoice(id)
-                    invoice.id = addInvoiceAndReturnId(jsonService.toJson(invoice)) as long
-                    return invoice
-                }
-            }
-
-
-    def "should return zero if there is no invoice"() {
-        when:
-        def taxCalculatorResponse = taxCalculatorService.calculateTaxes("0")
-
         then:
-        taxCalculatorResponse.income == 0
-        taxCalculatorResponse.costs == 0
-        taxCalculatorResponse.earnings == 0
-        taxCalculatorResponse.incomingVat == 0
-        taxCalculatorResponse.outgoingVat == 0
-        taxCalculatorResponse.vatToReturn == 0
-    }
-
-    def "should return sum of products if NIP matches"() {
-        given:
-        addInvoices(10)
-        when:
-        def taxCalculatorResponse = taxCalculatorService.calculateTaxes("5")
-
-        then:
+        def taxCalculatorResponse = jsonService.toObject(result, TaxCalculatorResult.class)
         taxCalculatorResponse.income == 5000
         taxCalculatorResponse.costs == 5000
         taxCalculatorResponse.earnings == 0
         taxCalculatorResponse.incomingVat == 400
         taxCalculatorResponse.outgoingVat == 400
+        taxCalculatorResponse.vatToReturn == 0
+    }
+
+    def "should return zero if there is no invoice"() {
+        given:
+        def url = "/tax/0"
+        when:
+        def result = mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andReturn()
+                .response
+                .contentAsString
+        then:
+        def taxCalculatorResponse = jsonService.toObject(result, TaxCalculatorResult.class)
+        taxCalculatorResponse.income == 0
+        taxCalculatorResponse.costs == 0
+        taxCalculatorResponse.earnings == 0
+        taxCalculatorResponse.incomingVat == 0
+        taxCalculatorResponse.outgoingVat == 0
         taxCalculatorResponse.vatToReturn == 0
     }
 }
